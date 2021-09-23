cfbe5b0a077272ba80a40c00dc4dc95cd0900b10
unknown <lhqbao@gmail.com>
2021-09-23 12:54:55 +0700

    make it better

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://jikbwkykewlhre:f25c22c74d50b4981449c23f2c1687e7b13f365bd0accbf177ca9c9d12f83bcd@ec2-54-174-172-218.compute-1.amazonaws.com:5432/df4ielomdmnsv5");
+	$Connect = pg_connect("postgres://tjzqsudbummnmv:db5287ac2406b89a2cef2041eff8e5f32ad8da0f5437c3316f35f39f5cb7a16a@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d61v9qm3uaqiof");
      //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
