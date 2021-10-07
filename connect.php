7912562d33d4eca9afb006f73099f848e40212cb
lhqbao <57691739+lhqbao@users.noreply.github.com>
2021-10-07 21:36:18 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://tjzqsudbummnmv:db5287ac2406b89a2cef2041eff8e5f32ad8da0f5437c3316f35f39f5cb7a16a@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d61v9qm3uaqiof");
+	//$Connect = pg_connect("postgres://tjzqsudbummnmv:db5287ac2406b89a2cef2041eff8e5f32ad8da0f5437c3316f35f39f5cb7a16a@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d61v9qm3uaqiof");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
