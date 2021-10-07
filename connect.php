c107d0b5ddcefef4875dca2146d10f927fc8f445
Le Bao <lhqbao@gmail.com>
2021-10-07 21:51:04 +0700

    up

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://tjzqsudbummnmv:db5287ac2406b89a2cef2041eff8e5f32ad8da0f5437c3316f35f39f5cb7a16a@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d61v9qm3uaqiof");
+	$Connect = pg_connect("postgres://jmdrhuyxeteupz:59d17582c5cf3f93d4c70701169649492bf1ca1c26e7be4c1fed36ee1c91e358@ec2-34-205-14-168.compute-1.amazonaws.com:5432/d2hk0ut4ifpeo7");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
